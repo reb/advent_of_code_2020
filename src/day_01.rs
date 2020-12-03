dfdfcd1f360598ba81dcfb79b9070d77f0185c45
reb <reboumans@gmail.com>
2020-12-03 18:41:00 -0500

    Find the two values in the expense report that sum to 2020 and return their multiplication

--- src/day_01.rs
+++ src/day_01.rs
@@ -38,8 +38,32 @@
 const INPUT: &str = include_str!("../input/day_01.txt");
 
 pub fn run() {
-    println!("Not implemented yet");
-    unimplemented!();
+    // sort the expense report to make finding two sums easier
+    let mut expense_report = parse_to_ints(INPUT);
+    expense_report.sort();
+
+    let (n1, n2) = find_sum(&expense_report, 2020);
+
+    println!(
+        "The two entries that sum to 2020 multiplied are {} * {} = {}",
+        n1,
+        n2,
+        n1 * n2
+    );
+}
+
+fn find_sum(expense_report: &Vec<u32>, target: u32) -> (u32, u32) {
+    for i in 0..expense_report.len() {
+        for j in i..expense_report.len() {
+            let sum = expense_report[i] + expense_report[j];
+            if sum == target {
+                return (expense_report[i], expense_report[j]);
+            } else if sum > target {
+                break;
+            }
+        }
+    }
+    panic!("No sum found!")
 }
 
 fn parse_to_ints(input: &str) -> Vec<u32> {
@@ -59,5 +83,14 @@ mod tests {
         let expected = vec![123, 456, 789];
 
         assert_eq!(parse_to_ints(input), expected);
+    }
+
+    #[test]
+    fn test_find_sum() {
+        let mut expense_report = vec![1721, 979, 366, 299, 675, 1456];
+        expense_report.sort();
+        let target = 2020;
+
+        assert_eq!(find_sum(&expense_report, target), (299, 1721));
     }
 }
