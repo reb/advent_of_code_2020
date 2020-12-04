416abe1d670a50c29ae42b018b644329442cd88a
reb <reboumans@gmail.com>
2020-12-04 17:12:51 -0500

    Minimize the probability of a sudden arboreal stop

--- src/day_03.rs
+++ src/day_03.rs
@@ -68,6 +68,27 @@
 ///
 /// Starting at the top-left corner of your map and following a slope of right 3
 /// and down 1, how many trees would you encounter?
+///
+/// --- Part Two ---
+///
+/// Time to check the rest of the slopes - you need to minimize the probability
+/// of a sudden arboreal stop, after all.
+///
+/// Determine the number of trees you would encounter if, for each of the
+/// following slopes, you start at the top-left corner and traverse the map all
+/// the way to the bottom:
+///
+///     Right 1, down 1.
+///     Right 3, down 1. (This is the slope you already checked.)
+///     Right 5, down 1.
+///     Right 7, down 1.
+///     Right 1, down 2.
+///
+/// In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+/// respectively; multiplied together, these produce the answer 336.
+///
+/// What do you get if you multiply together the number of trees encountered on
+/// each of the listed slopes?
 use std::collections::HashSet;
 use std::ops::AddAssign;
 
@@ -80,6 +101,16 @@ pub fn run() {
     println!(
         "Following a slope of right 3 and down 1, the amount of trees hit is: {}",
         trees_hit
+    );
+
+    let answer = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|&(right, down)| traverse(&map, right, down))
+        .map(|trees_hit| trees_hit as i64)
+        .product::<i64>();
+    println!(
+        "Multiplying the number of trees encountered for all slops gives: {}",
+        answer
     );
 }
 
@@ -195,12 +226,23 @@ mod tests {
         // #.##...#...
         // #...##....#
         // .#..#...#.#
-        let input = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
+        let input = "\
+            ..##.......\n\
+            #...#...#..\n\
+            .#....#..#.\n\
+            ..#.#...#.#\n\
+            .#...##..#.\n\
+            ..#.##.....\n\
+            .#.#.#....#\n\
+            .#........#\n\
+            #.##...#...\n\
+            #...##....#\n\
+            .#..#...#.#";
         // cheat a bit by using parse_map to build the map
         let map = parse_map(input);
 
         // traverse the trees with a slope of right 3, down 1 (the example from the description)
         // this should encounter 7 trees
-        assert_eq!(traverse(map, 3, 1), 7)
+        assert_eq!(traverse(&map, 3, 1), 7)
     }
 }
