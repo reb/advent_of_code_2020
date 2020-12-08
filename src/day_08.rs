7a875e2dca3fff4f5caefc5bcb8ce4af34745f26
reb <reboumans@gmail.com>
2020-12-08 16:19:04 -0500

    Run the program code

--- src/day_08.rs
+++ src/day_08.rs
@@ -66,10 +66,207 @@
 ///
 /// Run your copy of the boot code. Immediately before any instruction is
 /// executed a second time, what value is in the accumulator?
+use itertools::Itertools;
+use std::collections::HashSet;
+use Operation::*;
+use ProgramResult::*;
 
 const INPUT: &str = include_str!("../input/day_08.txt");
 
 pub fn run() {
-    println!("Not implemented yet");
-    unimplemented!();
+    let program = load_program(INPUT);
+
+    let (_, result_before_loop) = run_program(&program);
+    println!(
+        "The value of the accumulator before any instruction is executed again is: {}",
+        result_before_loop
+    )
+}
+
+fn run_program(program: &Vec<Instruction>) -> (ProgramResult, i32) {
+    let mut index = 0;
+    let mut accumulator = 0;
+    // keep track of visited location to prevent infinite loops
+    let mut visited = HashSet::new();
+
+    loop {
+        if index == program.len() {
+            return (Finished, accumulator);
+        }
+        if visited.contains(&index) {
+            return (InfiniteLoop, accumulator);
+        }
+        visited.insert(index);
+
+        match program.get(index) {
+            Some(Instruction {
+                operation: NoOperation,
+                ..
+            }) => {
+                index += 1;
+            }
+            Some(Instruction {
+                operation: Accumulate,
+                argument,
+            }) => {
+                index += 1;
+                accumulator += argument;
+            }
+            Some(Instruction {
+                operation: Jump,
+                argument,
+            }) => {
+                index = (index as i32 + argument) as usize;
+            }
+            _ => {
+                return (InstructionFailure, accumulator);
+            }
+        };
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Operation {
+    NoOperation,
+    Accumulate,
+    Jump,
+}
+
+#[derive(Debug, PartialEq)]
+struct Instruction {
+    operation: Operation,
+    argument: i32,
+}
+
+#[derive(Debug, PartialEq)]
+enum ProgramResult {
+    InfiniteLoop,
+    Finished,
+    InstructionFailure,
+}
+
+fn load_program(input: &str) -> Vec<Instruction> {
+    input
+        .split_whitespace()
+        .tuples::<(_, _)>()
+        .map(|(op, arg)| match (op, arg.parse()) {
+            ("nop", Ok(argument)) => Instruction {
+                operation: NoOperation,
+                argument,
+            },
+            ("acc", Ok(argument)) => Instruction {
+                operation: Accumulate,
+                argument,
+            },
+            ("jmp", Ok(argument)) => Instruction {
+                operation: Jump,
+                argument,
+            },
+            _ => panic!("Could not parse instruction"),
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_program() {
+        let input = "\
+            nop +0\n\
+            acc +1\n\
+            jmp +4\n\
+            acc +3\n\
+            jmp -3\n\
+            acc -99\n\
+            acc +1\n\
+            jmp -4\n\
+            acc +6";
+
+        let expected_program = vec![
+            Instruction {
+                operation: NoOperation,
+                argument: 0,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 1,
+            },
+            Instruction {
+                operation: Jump,
+                argument: 4,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 3,
+            },
+            Instruction {
+                operation: Jump,
+                argument: -3,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: -99,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 1,
+            },
+            Instruction {
+                operation: Jump,
+                argument: -4,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 6,
+            },
+        ];
+
+        assert_eq!(load_program(input), expected_program);
+    }
+
+    #[test]
+    fn test_run_program() {
+        let program = vec![
+            Instruction {
+                operation: NoOperation,
+                argument: 0,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 1,
+            },
+            Instruction {
+                operation: Jump,
+                argument: 4,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 3,
+            },
+            Instruction {
+                operation: Jump,
+                argument: -3,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: -99,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 1,
+            },
+            Instruction {
+                operation: Jump,
+                argument: -4,
+            },
+            Instruction {
+                operation: Accumulate,
+                argument: 6,
+            },
+        ];
+
+        assert_eq!(run_program(&program), (InfiniteLoop, 5));
+    }
 }
